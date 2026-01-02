00b5858b333ef246b01c752f207a578d8cb85cfc
Thiruvigraman <165885391+Thiruvigraman@users.noreply.github.com>
2026-01-02 20:46:29 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
 import { serve } from "https://deno.land/std@0.203.0/http/server.ts";
 
-serve(() => {
-  return new Response("BountyFlix is live ✅");
-});
+const PORT = Number(Deno.env.get("PORT") || 8000);
+
+console.log(`Listening on http://localhost:${PORT}`);
+
+serve(
+  () => new Response("BountyFlix is live ✅"),
+  { port: PORT }
+);
